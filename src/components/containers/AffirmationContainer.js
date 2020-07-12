86c4b534451c4fd33385f5a1bd89bf7cbc6eda7a
github username <github email address>
2020-07-13 00:43:16 +0100

    affirmation attempt

--- src/components/containers/AffirmationContainer.js
+++ src/components/containers/AffirmationContainer.js
@@ -6,17 +6,17 @@ class AffirmationContainer extends React.Component{
         affirmation: []
     }
 
-    // componentDidMount(){
-    //     fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium", {
-	//         "method": "GET",
-	//         "headers": {
-	// 	    "x-rapidapi-host": "healthruwords.p.rapidapi.com",
-	// 	    "x-rapidapi-key": "c3f5aaa52cmsh7c164162eddb056p121118jsn507985c3776f"
-    //     }}
-    // )
-    // .then(res => res.json())
-    // .then(json => this.setState({affirmation: json}))
-    // }
+    componentDidMount(){
+        fetch("https://healthruwords.p.rapidapi.com/v1/quotes", {
+	        "method": "GET",
+	        "headers": {
+		    "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+		    "x-rapidapi-key": "c3f5aaa52cmsh7c164162eddb056p121118jsn507985c3776f"
+        }}
+    )
+    .then(res => res.json())
+    .then(json => this.setState({affirmation: json}))
+    }
 
     render(){
         return <h2>universal affirmations</h2>
